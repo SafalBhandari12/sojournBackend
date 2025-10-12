1cca952e7af488acaf138b714187619ae38dcc09
SafalBhandari12 <safalbhandari069@gmail.com>
2025-10-12 22:38:07 +0530

    decreased rate-limited

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.use(
 // General API rate limiting
 const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 1000, // Limit each IP to 100 requests per windowMs
   message: {
     success: false,
     message: "Too many requests from this IP, please try again later.",
